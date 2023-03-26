55d9730aa07bdd1d6cea8b8a76b893d9b33ce1b9
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-25 19:37:56 -0700

    add views/machines/delete.cshtml and Delete functionality to MachinesController.cs

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -55,6 +55,21 @@ public class MachinesController : Controller
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+        public ActionResult Delete(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      _db.Machines.Remove(thisMachine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
       public ActionResult AddCertifications(int id)
     {
       Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
